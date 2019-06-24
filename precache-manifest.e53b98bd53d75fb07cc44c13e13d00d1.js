1d3a8ba5f8685f31488cd5427a3e55a82049a944
Zach Hollingsworth <zahollingsworth@Zachs-MacBook.local>
2019-06-23 20:24:04 -0400

    Updates

---
+++ precache-manifest.e53b98bd53d75fb07cc44c13e13d00d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ebd9478d6921a486bc97c696bf0502",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "13bb6a40559a7c1856ce",
+    "url": "/Clicky-Game/static/css/main.13f809b1.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/Clicky-Game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "13bb6a40559a7c1856ce",
+    "url": "/Clicky-Game/static/js/main.5c9b9369.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
